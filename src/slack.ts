8ddcd038faa27b5be18861612678792d8f4362cf
Tanner B. Hess Webber <tb@hesswebber.com>
2023-09-21 10:56:42 -0400

    feat(security): validates message is from slack

--- src/slack.ts
+++ src/slack.ts
@@ -1,9 +1,11 @@
 import type { Handler } from '@netlify/functions';
-import { slackApi } from './util/slack';
+import { isSlackEvent, slackApi } from './util/slack';
 import { parse } from 'querystring';
 
 export const handler: Handler = async (event) => {
-	// TODO validate the Slack request
+	if (!isSlackEvent(event)) {
+		return {statusCode: 400, body: "Invalid Request"}
+	}
 
 	const body = parse(event.body ?? "") as SlackPayload;
 	if (body.command) {
@@ -20,13 +22,16 @@ export const handler: Handler = async (event) => {
 
 async function handleSlashCommand(payload: SlackSlashCommandPayload) {
 	switch(payload.command) {
-		case "/fight": 
-		  const response = await slackApi("chat.postMessage", {channel: payload.channel_id, text: "Things are happening"})
+		case "/fight":{
+			const jokeResponse = await fetch("https://icanhazdadjoke.com", {headers: {accept: "text/plain"}}) 
+				const response = await slackApi("chat.postMessage", {channel: payload.channel_id, text: await jokeResponse.text()})
+				
+				if (!response.ok) {
+					console.log(response);
+				}
 
-		  if (!response.ok) {
-			console.log(response);
-		  }
 		  break;
+		}
 		default: 
 		  return {statusCode: 200, body: `Command "${payload.command}" is not recognized.`}
 	}
--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,3 +1,6 @@
+import { HandlerEvent } from "@netlify/functions";
+import { createHmac } from "crypto";
+
 export async function slackApi(endpoint:SlackApiEndpoint, body: SlackApiRequestBody) {
     const response = await fetch(`https://slack.com/api/${endpoint}`, {method: "POST", headers: {
         Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN ?? "Undefined Slack Bot Token"}`,
@@ -6,4 +9,26 @@ export async function slackApi(endpoint:SlackApiEndpoint, body: SlackApiRequestB
     body: JSON.stringify(body)});
 
     return response.json();
+}
+
+export async function isSlackEvent(event: HandlerEvent) {
+    const secret = process.env.SLACK_SIGNING_SECRET;
+    const signature = event.headers['x-slack-signature'];
+    const timestamp = Number(event.headers['x-slack-request-timestampe']);
+    const currentTime = Math.floor(Date.now() / 1000);
+
+    if (Math.abs(currentTime - timestamp) > 300) {
+        return false;
+    }
+
+    if (!secret) {
+        console.error("Slack signing secret cannot be undefined");
+        return false;
+    }
+
+    const hash = createHmac("sha256", secret)
+        .update(`v0:${timestamp}:${event.body}`)
+        .digest("hex");
+
+    return `v0=${hash}` === signature;
 }
