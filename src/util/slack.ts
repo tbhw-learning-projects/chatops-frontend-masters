afb7daa67816ec5aeb8d90cc2bd93de5b9810dbf
Tanner B. Hess Webber <tb@hesswebber.com>
2023-09-21 10:36:58 -0400

    feat(hello world): wires up bot to slack

--- src/util/slack.ts
+++ src/util/slack.ts
@@ -1,1 +1,9 @@
-// TODO create Slack utilities
+export async function slackApi(endpoint:SlackApiEndpoint, body: SlackApiRequestBody) {
+    const response = await fetch(`https://slack.com/api/${endpoint}`, {method: "POST", headers: {
+        Authorization: `Bearer ${process.env.SLACK_BOT_OAUTH_TOKEN ?? "Undefined Slack Bot Token"}`,
+        'Content-Type': "application/json; charset=utf-8"
+    },
+    body: JSON.stringify(body)});
+
+    return response.json();
+}
